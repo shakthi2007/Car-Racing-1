53177c5f8b1499206ce9c057f13d40c74d8b51e6
shakthi2007 <63248034+shakthi2007@users.noreply.github.com>
2020-06-08 20:55:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+ var database,game,form;
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+  game=new Game();
+  game.start();
+}
+
+function draw(){
+  background("white");
+    drawSprites();
+  
+}
+
+
